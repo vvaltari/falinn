eae25be83bc8bf01cf99c88baa5acdae33686e1b
lj0s-sh <studiesec.hula878@passmail.net>
2025-06-23 10:42:32 -0300

    feat: add user models

--- schemas.py
+++ schemas.py
@@ -1,9 +1,23 @@
-from pydantic import BaseModel, HttpUrl, Field, ConfigDict
+from pydantic import BaseModel, HttpUrl, Field, ConfigDict, EmailStr
 from pydantic.functional_validators import BeforeValidator
 from bson import ObjectId
 from typing import Optional, Annotated
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class UserModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id', default=None)
+    name: str = Field(...)
+    last_name: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True
+    )
+
+class UserCollection(BaseModel):
+    users: list[UserModel]
 
 class SecretModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id', default=None)
