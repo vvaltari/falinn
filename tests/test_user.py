57ce8d3d7ebe988c5b4830ebf1dd7bd3182ea898
lj0s-sh <studiesec.hula878@passmail.net>
2025-07-10 13:00:19 -0300

    refactor: change test user fixture to a dictionary

--- tests/test_user.py
+++ tests/test_user.py
@@ -11,7 +11,7 @@ from src.dependencies import get_db
 
 from src.users.schemas import UserModel
 
-from src.auth.utils import verify_password, create_access_token
+from src.auth.utils import hash_password, verify_password, create_access_token
 from src.auth.dependencies import validate_token
 
 from src.main import app
@@ -43,15 +43,17 @@ async def test_user(test_db):
         'name': 'Test',
         'last_name': 'Test',
         'email': 'test@example.com',
-        'password': '$2b$12$YSDci3yw1fDzrRSrj8kyB.yDUThygQWQod7fVqhzB/1kDPAunJGPy'
+        'unhashed_password': 'test_password'
     }
 
+    user_data['password'] = hash_password(user_data['unhashed_password'])
+
     await test_db['users'].insert_one(user_data)
-    return UserModel(**user_data)
+    return user_data
 
 @pytest_asyncio.fixture(autouse=True)
 async def test_token(test_user):
-    token = create_access_token({ 'sub': test_user.id })
+    token = create_access_token({ 'sub': str(test_user['_id']) })
     return token
 
 @pytest_asyncio.fixture
@@ -60,25 +62,6 @@ async def client(test_db):
     async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as ac:
         yield ac
     app.dependency_overrides.clear()
-
-# @pytest.mark.asyncio
-# async def test_sign_up(client, test_db):
-#     payload = {
-#         'name': 'Test User',
-#         'last_name': 'Test User',
-#         'email': 'test@example.com',
-#         'password': '123',
-#     }
-
-#     response = await client.post("/sign-in", json=payload)
-#     assert response.status_code == 201
-
-#     user = await test_db["users"].find_one({ "email": "test@example.com" })
-
-#     assert user is not None
-#     assert user["name"] == "Test User"
-#     assert user["last_name"] == "Test User"
-#     assert verify_password(payload['password'], user['password'])
 
 @pytest.mark.asyncio
 async def test_me(client, test_token):
@@ -98,7 +81,7 @@ async def test_me_invalid_token(client):
 
 @pytest.mark.asyncio
 async def test_me_deleted_user_with_valid_token(client, test_db, test_user, test_token):
-    await test_db['users'].delete_one({ '_id': ObjectId(test_user.id) })
+    await test_db['users'].delete_one({ '_id': test_user['_id'] })
     
     headers = { 'Authorization': f'Bearer {test_token}'}
 
@@ -118,13 +101,13 @@ async def test_update_user(client, test_db, test_user, test_token):
 
     response = await client.put('/users/', json=data, headers=headers)
 
-    update_user = await test_db['users'].find_one({ '_id': ObjectId(test_user.id) })
+    update_user = await test_db['users'].find_one({ '_id': test_user['_id'] })
 
     assert response.status_code == 200
     assert update_user['name'] == data['name']
     assert update_user['last_name'] == data['last_name']
     assert update_user['email'] == data['email']
-    assert update_user['password'] == test_user.password
+    assert update_user['password'] == test_user['password']
 
 @pytest.mark.asyncio
 async def test_update_user_with_different_password(client, test_db, test_user, test_token):
@@ -136,10 +119,10 @@ async def test_update_user_with_different_password(client, test_db, test_user, t
 
     response = await client.put('/users/', json=data, headers=headers)
 
-    update_user = await test_db['users'].find_one({ '_id': ObjectId(test_user.id) })
+    update_user = await test_db['users'].find_one({ '_id': test_user['_id'] })
 
     assert response.status_code == 200
-    assert update_user['password'] != test_user.password
+    assert update_user['password'] != test_user['password']
     assert verify_password(data['password'], update_user['password']) == True
 
 @pytest.mark.asyncio
@@ -153,15 +136,15 @@ async def test_update_user_empty_request(client, test_user, test_token):
     user = response.json()
 
     assert response.status_code == 200
-    assert user['id'] == test_user.id
-    assert user['name'] == test_user.name
-    assert user['last_name'] == test_user.name
-    assert user['email'] == test_user.email
+    assert user['id'] == str(test_user['_id'])
+    assert user['name'] == test_user['name']
+    assert user['last_name'] == test_user['last_name']
+    assert user['email'] == test_user['email']
 
 @pytest.mark.asyncio
 async def test_update_user_not_found(client, test_token, test_user, test_db):
-    app.dependency_overrides[validate_token] = lambda: test_user
-    await test_db['users'].delete_one({ '_id': ObjectId(test_user.id) })
+    app.dependency_overrides[validate_token] = lambda: UserModel(**test_user)
+    await test_db['users'].delete_one({ '_id': test_user['_id'] })
 
     data = {
         'name': 'Update test',
@@ -179,8 +162,8 @@ async def test_update_user_not_found(client, test_token, test_user, test_db):
 
 @pytest.mark.asyncio
 async def test_update_user_empty_request_user_not_found(client, test_token, test_user, test_db):
-    app.dependency_overrides[validate_token] = lambda: test_user
-    await test_db['users'].delete_one({ '_id': ObjectId(test_user.id) })
+    app.dependency_overrides[validate_token] = lambda: UserModel(**test_user)
+    await test_db['users'].delete_one({ '_id': test_user['_id'] })
 
     data = {}
 
@@ -199,15 +182,15 @@ async def test_delete_user(client, test_token, test_user, test_db):
 
     response = await client.delete('/users/', headers=headers)
 
-    user = await test_db['users'].find_one({ '_id': ObjectId(test_user.id) })
+    user = await test_db['users'].find_one({ '_id': test_user['_id'] })
 
     assert response.status_code == 204
     assert user == None
 
 @pytest.mark.asyncio
 async def test_delete_user_valid_token_deleted_user(client, test_token, test_user, test_db):
-    app.dependency_overrides[validate_token] = lambda: test_user
-    await test_db['users'].delete_one({ '_id': ObjectId(test_user.id) })
+    app.dependency_overrides[validate_token] = lambda: UserModel(**test_user)
+    await test_db['users'].delete_one({ '_id': test_user['_id'] })
     
     headers = { 'Authorization': f'Bearer {test_token}' }
 
