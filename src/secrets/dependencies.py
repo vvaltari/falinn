6590c57c091846e137a02f2f07f4105cc6d9ea8d
lj0s-sh <studiesec.hula878@passmail.net>
2025-07-14 11:21:09 -0300

    fix: fix secrets collection

--- src/secrets/dependencies.py
+++ src/secrets/dependencies.py
@@ -2,4 +2,4 @@ from fastapi import Depends
 from src.dependencies import get_db
 
 async def get_secret_collection(db = Depends(get_db)):
-    return db.get_collection['secets']
+    return db.get_collection('secrets')
